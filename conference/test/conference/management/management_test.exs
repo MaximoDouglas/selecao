2584b6aeb1eb62e2121be348a3ca5c2eb90e3750
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-04-15 12:31:41 -0300

    Test of return and test if name contains numbers

--- conference/test/conference/management/management_test.exs
+++ conference/test/conference/management/management_test.exs
@@ -1,65 +1,20 @@
 defmodule Conference.ManagementTest do
   use Conference.DataCase
-
   alias Conference.Management
+  
+  @doc """
+    author: MaximoDouglas
+  """
+  describe "management_module" do
+    # File with numbers in its speeche names
+    @mock_file_path "/home/douglas/dev/git/selecao/mock.proposals.txt"
+    
+    # File without numbers in its speeche names
+    @file_path "/home/douglas/dev/git/selecao/proposals.txt"
 
-  describe "tracks" do
-    alias Conference.Management.Track
-
-    @valid_attrs %{name: "some name"}
-    @update_attrs %{name: "some updated name"}
-    @invalid_attrs %{name: nil}
-
-    def track_fixture(attrs \\ %{}) do
-      {:ok, track} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Management.create_track()
-
-      track
-    end
-
-    test "list_tracks/0 returns all tracks" do
-      track = track_fixture()
-      assert Management.list_tracks() == [track]
-    end
-
-    test "get_track!/1 returns the track with given id" do
-      track = track_fixture()
-      assert Management.get_track!(track.id) == track
-    end
-
-    test "create_track/1 with valid data creates a track" do
-      assert {:ok, %Track{} = track} = Management.create_track(@valid_attrs)
-      assert track.name == "some name"
-    end
-
-    test "create_track/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Management.create_track(@invalid_attrs)
-    end
-
-    test "update_track/2 with valid data updates the track" do
-      track = track_fixture()
-      assert {:ok, track} = Management.update_track(track, @update_attrs)
-      assert %Track{} = track
-      assert track.name == "some updated name"
-    end
-
-    test "update_track/2 with invalid data returns error changeset" do
-      track = track_fixture()
-      assert {:error, %Ecto.Changeset{}} = Management.update_track(track, @invalid_attrs)
-      assert track == Management.get_track!(track.id)
-    end
-
-    test "delete_track/1 deletes the track" do
-      track = track_fixture()
-      assert {:ok, %Track{}} = Management.delete_track(track)
-      assert_raise Ecto.NoResultsError, fn -> Management.get_track!(track.id) end
-    end
-
-    test "change_track/1 returns a track changeset" do
-      track = track_fixture()
-      assert %Ecto.Changeset{} = Management.change_track(track)
+    test "file_to_speeches/1 return speeches withou numbers in its names" do
+      speeches = Management.file_to_speeches(@file_path)
+      assert Management.file_to_speeches(@mock_file_path) == speeches
     end
   end
 end
